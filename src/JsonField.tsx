5879c508152eaf8ab436f71fa83ea3afa667c845
Grégoire Hertault <ghertault@gmail.com>
2020-12-23 14:57:29 +0100

    Improve types (#3)

--- src/JsonField.tsx
+++ src/JsonField.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { FieldProps } from "react-admin";
 import ReactJson, { ReactJsonViewProps } from "react-json-view";
 
 type Props = {
   source: string;
   record?: any;
   reactJsonOptions?: ReactJsonViewProps;
-};
+} & FieldProps;
 
 export const JsonField: React.FC<Props> = ({
   source,
