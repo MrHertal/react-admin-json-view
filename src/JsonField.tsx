f12f554dc3dec346575f1ca91c530cabed68d969
Grégoire Hertault <ghertault@gmail.com>
2020-12-28 12:21:27 +0100

    Support nested source (#5)

--- src/JsonField.tsx
+++ src/JsonField.tsx
@@ -1,3 +1,4 @@
+import get from "lodash/get";
 import React from "react";
 import { FieldProps } from "react-admin";
 import ReactJson, { ReactJsonViewProps } from "react-json-view";
@@ -13,5 +14,7 @@ export const JsonField: React.FC<Props> = ({
   record = {},
   reactJsonOptions = {},
 }) => {
-  return <ReactJson {...reactJsonOptions} src={record[source] || {}} />;
+  const value = get(record, source);
+
+  return <ReactJson {...reactJsonOptions} src={value || {}} />;
 };
